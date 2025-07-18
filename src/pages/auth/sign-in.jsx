a6d0873a349d5f7618a2f129f3d5630689b05d3e
anton <anton@BOOK-0E4F8KFOGV>
2025-07-18 13:22:46 +0300

    re

--- src/pages/auth/sign-in.jsx
+++ src/pages/auth/sign-in.jsx
@@ -42,10 +42,12 @@ export function SignIn() {
   };
 
   return (
-    <section className="about-us-page m-8 flex gap-4">
-              <div className="capitalize bg-gray rounded-lg px-8 py-2">
-          { <Typography variant="h6" >
-          <svg xmlns="http://www.w3.org/2000/svg" width="211" height="40" viewBox="0 0 211 40" fill="none">
+    <section className="min-h-screen flex flex-col lg:flex-row bg-gray-50">
+      {/* Logo Section - Mobile Top, Desktop Left */}
+      <div className="w-full lg:w-auto flex justify-center lg:justify-start p-4 lg:p-8">
+        <div className="capitalize bg-gray-100 rounded-lg px-4 py-2 lg:px-8 lg:py-2">
+          <Typography variant="h6" className="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" width="160" height="30" viewBox="0 0 211 40" fill="none" className="lg:w-[211px] lg:h-[40px]">
             <path d="M90.3999 10C90.3999 4.5 94.8999 0 100.4 0C105.9 0 110.4 4.5 110.4 10C110.4 15.5 105.9 20 100.4 20C94.8999 20.1 90.3999 15.6 90.3999 10Z" fill="#B4B239"></path>
             <path d="M99.6002 13.9999L102.4 10.8999L102 10.9999H94.2002V9.0999H102L102.4 9.1999L99.6002 6.0999L101 4.8999L105.6 9.9999L101 15.1999L99.6002 13.9999Z" fill="white"></path>
             <path d="M122.2 16.4V14.9C122.1 15.9 121.6 16.4 120.7 16.4H119.2C118.4 16.4 117.6 16.1 117 15.5C116.4 14.9 116.1 14.2 116.1 13.4V8.79995C116.1 7.99995 116.4 7.29995 117 6.59995C117.6 5.99995 118.3 5.69995 119.2 5.69995H122.3V2.69995H125.3V16.4H122.2ZM119.6 7.79995C119.3 8.09995 119.1 8.49995 119.1 8.89995V13.5C119.1 13.9 119.2 14.3 119.6 14.6C119.9 14.9 120.3 15.1 120.7 15.1C121.1 15.1 121.5 15 121.8 14.6C122.1 14.3 122.3 14 122.3 13.5V7.39995H120.8C120.3 7.29995 119.9 7.39995 119.6 7.79995Z" fill="#B4B239"></path>
@@ -63,15 +65,22 @@ export function SignIn() {
             <path d="M65.2998 39.9V16.5H70.4998V39.9H65.2998Z" fill="#888888"></path>
             <path d="M89.9999 21.6999H87.3999C87.1999 19.9999 86.3999 19.0999 84.8999 19.0999C83.9999 19.0999 83.2999 19.2999 82.7999 19.7999C82.2999 20.1999 82.0999 20.8999 82.0999 21.7999C82.0999 22.4999 82.3999 23.0999 82.8999 23.5999L88.3999 29.7999C89.3999 30.8999 89.8999 32.1999 89.8999 33.4999V34.7999C89.8999 36.1999 89.3999 37.3999 88.3999 38.4999C87.3999 39.4999 86.1999 39.9999 84.6999 39.9999H82.0999C80.3999 39.9999 79.0999 39.5999 78.1999 38.6999C77.2999 37.7999 76.8999 36.4999 76.8999 34.7999H79.4999C79.4999 34.9999 79.4999 35.0999 79.4999 35.1999C79.4999 35.8999 79.7999 36.4999 80.2999 36.7999C80.7999 37.1999 81.4999 37.2999 82.0999 37.2999C82.9999 37.2999 83.6999 37.0999 84.0999 36.5999C84.4999 36.1999 84.6999 35.4999 84.6999 34.5999C84.6999 33.8999 84.3999 33.2999 83.8999 32.7999L78.3999 26.5999C77.3999 25.4999 76.8999 24.1999 76.8999 22.8999V21.5999C76.8999 20.1999 77.3999 18.9999 78.3999 17.8999C79.3999 16.8999 80.5999 16.3999 82.0999 16.3999H84.6999C88.0999 16.4999 89.7999 18.1999 89.9999 21.6999Z" fill="#888888"></path>
           </svg>
-          </Typography> }
+          </Typography>
         </div>
-      <div className="w-full lg:w-3/5 mt-24">
+      </div>
+
+      {/* Form Section - Mobile Below Logo, Desktop Center */}
+      <div className="w-full lg:w-3/5 flex flex-col justify-center px-4 py-8 lg:px-12 lg:py-24">
       
         <div className="text-center">
-          <h1 className="display-3 fw-bold mb-4">Sign In</h1>
-          <p className="lead text-blue-gray-700 mb-6">Enter your username and password to Sign In.</p>
+          <Typography variant="h2" className="text-2xl lg:text-4xl font-bold mb-4">
+            Sign In
+          </Typography>
+          <Typography className="text-lg text-blue-gray-700 mb-6">
+            Enter your username and password to Sign In.
+          </Typography>
         </div>
-        <form className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2" onSubmit={handleSubmit}>
+        <form className="mt-8 mb-2 mx-auto w-full max-w-md lg:max-w-lg" onSubmit={handleSubmit}>
           <div className="mb-1 flex flex-col gap-6">
             <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
               Username
@@ -132,7 +141,7 @@ export function SignIn() {
               Signin successful!
             </Typography>
           )}
-          <div className="flex items-center justify-between gap-2 mt-6">
+          <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mt-6">
             <Checkbox
               label={
                 <Typography
@@ -146,43 +155,24 @@ export function SignIn() {
               containerProps={{ className: "-ml-2.5" }}
             />
             <Typography variant="small" className="font-medium text-gray-900">
-              <a href="#">
+              <a href="#" className="hover:underline">
                 Forgot Password
               </a>
             </Typography>
           </div>
-          <div className="space-y-4 mt-8">
-            {/* <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth>
-              <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clipPath="url(#clip0_1156_824)">
-                  <path d="M16.3442 8.18429C16.3442 7.64047 16.3001 7.09371 16.206 6.55872H8.66016V9.63937H12.9813C12.802 10.6329 12.2258 11.5119 11.3822 12.0704V14.0693H13.9602C15.4741 12.6759 16.3442 10.6182 16.3442 8.18429Z" fill="#4285F4" />
-                  <path d="M8.65974 16.0006C10.8174 16.0006 12.637 15.2922 13.9627 14.0693L11.3847 12.0704C10.6675 12.5584 9.7415 12.8347 8.66268 12.8347C6.5756 12.8347 4.80598 11.4266 4.17104 9.53357H1.51074V11.5942C2.86882 14.2956 5.63494 16.0006 8.65974 16.0006Z" fill="#34A853" />
-                  <path d="M4.16852 9.53356C3.83341 8.53999 3.83341 7.46411 4.16852 6.47054V4.40991H1.51116C0.376489 6.67043 0.376489 9.33367 1.51116 11.5942L4.16852 9.53356Z" fill="#FBBC04" />
-                  <path d="M8.65974 3.16644C9.80029 3.1488 10.9026 3.57798 11.7286 4.36578L14.0127 2.08174C12.5664 0.72367 10.6469 -0.0229773 8.65974 0.000539111C5.63494 0.000539111 2.86882 1.70548 1.51074 4.40987L4.1681 6.4705C4.8001 4.57449 6.57266 3.16644 8.65974 3.16644Z" fill="#EA4335" />
-                </g>
-                <defs>
-                  <clipPath id="clip0_1156_824">
-                    <rect width="16" height="16" fill="white" transform="translate(0.5)" />
-                  </clipPath>
-                </defs>
-              </svg>
-              <span>Sign in With Google</span>
-            </Button>
-            <Button size="lg" color="white" className="flex items-center gap-2 justify-center shadow-md" fullWidth>
-              <img src="/img/twitter-logo.svg" height={24} width={24} alt="" />
-              <span>Sign in With Twitter</span>
-            </Button> */}
-          </div>
           <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4">
             Not registered?
-            <Link to="/auth/sign-up" className="text-gray-900 ml-1">Create account</Link>
+            <Link to="/auth/sign-up" className="text-gray-900 ml-1 hover:underline">Create account</Link>
           </Typography>
         </form>
       </div>
+
+      {/* Background Image Section - Desktop Only */}
       <div className="w-2/5 h-full hidden lg:block">
         <img
           src="/img/pattern.png"
           className="h-full w-full object-cover rounded-3xl"
+          alt="Background pattern"
         />
       </div>
     </section>
